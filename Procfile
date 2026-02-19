53dd280a76b23dc2bd294674b887567009d1cf90
Harsh <harshadkedari211@gmail.com>
2026-02-19 19:20:13 +0530

    Fix Render deployment: Add build script and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn core.wsgi --threads 4 --log-file -
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p staticfiles
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
